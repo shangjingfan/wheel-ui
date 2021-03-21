8f57180cd58977b82253dc00edd74fbbbd313ee7
shangjingfan <876625909@qq.com>
2021-03-21 21:56:49 +0800

    Switch css 2

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -25,7 +25,7 @@ button {
   height: $h;
   width: $h * 2;
   border: none;
-  background: gray;
+  background: #bfbfbf;
   border-radius: $h/2;
   position: relative;
   transition: 0.5s;
@@ -41,12 +41,23 @@ span {
   transition: 0.5s;
 }
 button.checked {
-  background: blue;
+  background: #1890ff;
 }
 button.checked > span {
   left: calc(100% - #{$h2} - 2px);
 }
 button:focus {
   outline: none;
+}
+button:active {
+  > span {
+    width: $h2 + 4px;
+  }
+}
+button.checked:active {
+  > span {
+    width: $h2 + 4px;
+    margin-left: -4px;
+  }
 }
 </style>
