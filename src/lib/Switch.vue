cc024177d6928a00f0df5f98c699e79b1d717575
shangjingfan <876625909@qq.com>
2021-03-21 14:40:33 +0800

    hover 后按钮移动

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -9,17 +9,20 @@ button {
   height: $h;
   width: $h * 2;
   border: none;
-  background: #64bd63;
+  background: blue;
   border-radius: $h/2;
   position: relative;
 }
 span {
   position: absolute;
   top: 2px;
-  left: 2px;
+  left: calc(100% - #{$h2} - 2px);
   height: $h2;
   width: $h2;
   background: #ffffff;
   border-radius: $h2/2;
+}
+button:hover > span {
+  left: 2px;
 }
 </style>
