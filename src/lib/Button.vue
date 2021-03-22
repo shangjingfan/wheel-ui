793f9a4ca2930d33c1c6c114e7ef597db0794447
shangjingfan <876625909@qq.com>
2021-03-22 21:16:04 +0800

    支持loading

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,11 @@
 <template>
-  <button class="wheel-button" :class="classes" :disabled="disabled">
+  <button
+    class="wheel-button"
+    :class="classes"
+    :disabled="disabled"
+    :loading="loading"
+  >
+    <span v-if="loading" class="wheel-loadingIndicator"></span>
     <slot />
   </button>
 </template>
@@ -20,6 +26,10 @@ export default {
       default: "normal",
     },
     disabled: {
+      type: Boolean,
+      default: false,
+    },
+    loading: {
       type: Boolean,
       default: false,
     },
@@ -164,6 +174,25 @@ $grey: grey;
       cursor: not-allowed;
       color: $grey;
     }
+  }
+  > .wheel-loadingIndicator {
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: wheel-spin 1s infinite linear;
+  }
+}
+@keyframes wheel-spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
   }
 }
 </style>
