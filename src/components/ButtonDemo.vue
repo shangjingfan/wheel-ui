47aa5ba94228901b63473ebc87091807d22a857a
shangjingfan <876625909@qq.com>
2021-03-22 13:55:35 +0800

    基础样式

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,7 +2,7 @@
   <div>Button 示例</div>
   <h1>示例1</h1>
   <div>
-    <Button> 默认button </Button>
+    <Button> 默认 </Button>
     <Button theme="button"> button </Button>
     <Button theme="link"> link </Button>
     <Button theme="text"> text </Button>
