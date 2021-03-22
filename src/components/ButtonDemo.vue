c32cd951e312cda28697014005be8a3172670cf7
shangjingfan <876625909@qq.com>
2021-03-22 10:50:44 +0800

    创建Button组件，slot插槽,Vue3属性绑定

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,6 +1,23 @@
 <template>
+  <div>Button 示例</div>
+  <h1>示例1</h1>
   <div>
-    <p>Button 文档</p>
-    <p>Button 文档</p>
+    <Button @click="onClick" @focus="onClick" @mouseover="onClick">
+      你好
+    </Button>
   </div>
 </template>
+<script lang="ts">
+import Button from "../lib/Button.vue";
+export default {
+  components: {
+    Button,
+  },
+  setup() {
+    const onClick = () => {
+      console.log("helo");
+    };
+    return { onClick };
+  },
+};
+</script>
---
+++ src/lib/Button.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button v-bind="rest">
+      <slot />
+    </button>
+    <button @mouseover="onMouseover">11223</button>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  inheritAttrs: false,
+  setup(props, context) {
+    const { onMouseover, ...rest } = context.attrs;
+    return { onMouseover, rest };
+  },
+};
+</script>
+<style lang="scss" scoped>
+div {
+  border: 1px solid blue;
+}
+</style>
