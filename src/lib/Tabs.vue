87b9408decf4d5635f628bb9e05ac27e9737b2d8
shangjingfan <876625909@qq.com>
2021-03-23 15:51:05 +0800

    Tabs css

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,6 +1,23 @@
 <template>
-  <div v-for="(t, index) in titles" :key="index">{{ t }}</div>
-  <component v-for="(c, index) in defaults" :is="c" :key="index" />
+  <div class="wheel-tabs">
+    <div class="wheel-tabs-nav">
+      <div
+        class="wheel-tabs-nav-item"
+        v-for="(t, index) in titles"
+        :key="index"
+      >
+        {{ t }}
+      </div>
+    </div>
+    <div class="wheel-tabs-content">
+      <component
+        class="wheel-tabs-content-item"
+        v-for="(c, index) in defaults"
+        :is="c"
+        :key="index"
+      />
+    </div>
+  </div>
 </template>
 <script lang="ts">
 import Tab from "./Tab.vue";
@@ -19,3 +36,29 @@ export default {
   },
 };
 </script>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.wheel-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
