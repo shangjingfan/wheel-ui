3a89b80e1768a1864a514b62bcd645c56634b3e3
shangjingfan <876625909@qq.com>
2021-03-24 12:43:51 +0800

    优化2：watchEffect，类似React的useEffect

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -29,7 +29,7 @@
   </div>
 </template>
 <script lang="ts">
-import { computed, onMounted, onUpdated, ref } from "vue";
+import { computed, onMounted, onUpdated, ref, watchEffect } from "vue";
 import Tab from "./Tab.vue";
 export default {
   props: {
@@ -41,17 +41,15 @@ export default {
     const selectedItem = ref<HTMLDivElement>(null);
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
-    const x = () => {
+
+    watchEffect(() => {
       const { width } = selectedItem.value.getBoundingClientRect();
       indicator.value.style.width = width + "px";
       const { left: left1 } = container.value.getBoundingClientRect();
       const { left: left2 } = selectedItem.value.getBoundingClientRect();
       const left = left2 - left1;
       indicator.value.style.left = left + "px";
-    };
-
-    onMounted(x);
-    onUpdated(x);
+    });
 
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
