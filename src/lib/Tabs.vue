13d801880f6d88657b567d5e6b5e20c820e8c6dc
shangjingfan <876625909@qq.com>
2021-03-24 12:37:52 +0800

    优化1:直接获取selected

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -7,7 +7,7 @@
         v-for="(t, index) in titles"
         :ref="
           (el) => {
-            if (el) navItems[index] = el;
+            if (t === selected) selectedItem = el;
           }
         "
         @click="select(t)"
@@ -38,18 +38,14 @@ export default {
     },
   },
   setup(props, context) {
-    const container = ref<HTMLDivElement>(null);
-    const navItems = ref<HTMLDivElement[]>([]);
+    const selectedItem = ref<HTMLDivElement>(null);
     const indicator = ref<HTMLDivElement>(null);
+    const container = ref<HTMLDivElement>(null);
     const x = () => {
-      const divs = navItems.value;
-      const result = divs.filter((div) =>
-        div.classList.contains("selected")
-      )[0];
-      const { width } = result.getBoundingClientRect();
+      const { width } = selectedItem.value.getBoundingClientRect();
       indicator.value.style.width = width + "px";
       const { left: left1 } = container.value.getBoundingClientRect();
-      const { left: left2 } = result.getBoundingClientRect();
+      const { left: left2 } = selectedItem.value.getBoundingClientRect();
       const left = left2 - left1;
       indicator.value.style.left = left + "px";
     };
@@ -80,9 +76,9 @@ export default {
       titles,
       current,
       select,
-      navItems,
       indicator,
       container,
+      selectedItem,
     };
   },
 };
