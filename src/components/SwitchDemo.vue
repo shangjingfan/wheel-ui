614e571013dee18ffe34c1827c668e19b1934164
shangjingfan <876625909@qq.com>
2021-03-26 16:44:30 +0800

    删掉没用代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -3,26 +3,17 @@
   <Demo :component="Switch2Demo" />
 </template>
 <script lang="ts">
-import { ref } from "vue";
-import Switch from "../lib/Switch.vue";
-import Button from "../lib/Button.vue";
 import Switch1Demo from "../components/Switch1.demo.vue";
 import Switch2Demo from "../components/Switch2.demo.vue";
-import "prismjs";
-import "prismjs/themes/prism.css";
-const Prism = (window as any).Prism;
 
 import Demo from "../components/Demo.vue";
 
 export default {
   components: {
-    Switch,
-    Button,
     Demo,
   },
   setup() {
-    const bool = ref(false);
-    return { bool, Switch1Demo, Switch2Demo, Prism };
+    return { Switch1Demo, Switch2Demo };
   },
 };
 </script>
