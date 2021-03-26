a68071bf1959a620c93137e1b8111e747d815a6f
shangjingfan <876625909@qq.com>
2021-03-26 15:35:17 +0800

    展示源代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -10,7 +10,7 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>&lt;Switch v-model:value="bool"/&gt;</pre>
+        <pre>{{ Switch1Demo.__sourceCode }}</pre>
       </div>
     </div>
     <div class="demo">
@@ -22,7 +22,7 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
+        <pre>{{ Switch2Demo.__sourceCode }}</pre>
       </div>
     </div>
   </div>
@@ -31,18 +31,19 @@
 import { ref } from "vue";
 import Switch from "../lib/Switch.vue";
 import Button from "../lib/Button.vue";
-import Switch1demo from "../components/Switch1.demo.vue";
-import Switch2demo from "../components/Switch2.demo.vue";
+import Switch1Demo from "../components/Switch1.demo.vue";
+import Switch2Demo from "../components/Switch2.demo.vue";
+console.log(Switch1Demo.__sourceCode);
 export default {
   components: {
     Switch,
     Button,
-    Switch1demo,
-    Switch2demo,
+    Switch1Demo,
+    Switch2Demo,
   },
   setup() {
     const bool = ref(false);
-    return { bool };
+    return { bool, Switch1Demo, Switch2Demo };
   },
 };
 </script>
