7de351143c7e9b35ee67cb6347e03facc17dac0f
shangjingfan <876625909@qq.com>
2021-03-26 15:57:08 +0800

    高亮源代码，使用prism库

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -10,7 +10,16 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>{{ Switch1Demo.__sourceCode }}</pre>
+        <pre
+          class="language-html"
+          v-html="
+            Prism.highlight(
+              Switch1Demo.__sourceCode,
+              Prism.languages.html,
+              'html'
+            )
+          "
+        />
       </div>
     </div>
     <div class="demo">
@@ -22,7 +31,16 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>{{ Switch2Demo.__sourceCode }}</pre>
+        <pre
+          class="language-html"
+          v-html="
+            Prism.highlight(
+              Switch2Demo.__sourceCode,
+              Prism.languages.html,
+              'html'
+            )
+          "
+        />
       </div>
     </div>
   </div>
@@ -33,7 +51,10 @@ import Switch from "../lib/Switch.vue";
 import Button from "../lib/Button.vue";
 import Switch1Demo from "../components/Switch1.demo.vue";
 import Switch2Demo from "../components/Switch2.demo.vue";
-console.log(Switch1Demo.__sourceCode);
+import "prismjs";
+import "prismjs/themes/prism.css";
+const Prism = (window as any).Prism;
+
 export default {
   components: {
     Switch,
@@ -41,7 +62,7 @@ export default {
   },
   setup() {
     const bool = ref(false);
-    return { bool, Switch1Demo, Switch2Demo };
+    return { bool, Switch1Demo, Switch2Demo, Prism };
   },
 };
 </script>
