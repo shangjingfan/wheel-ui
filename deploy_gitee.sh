3678242af4ec33b1a5820ba51fd166d4323d833d
shangjingfan <876625909@qq.com>
2021-03-28 16:27:13 +0800

    码云一键部署

---
+++ deploy_gitee.sh
@@ -0,0 +1,9 @@
+rm -rf dist &&
+yarn build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m "update" &&
+git branch -M master &&
+git remote add origin git@gitee.com:shangjingfan/wheel-ui-website.git &&
+git push -f -u origin master  
