4c338b960a30c0b1fe3c07645d832f3cc6dccca7
shangjingfan <876625909@qq.com>
2021-03-30 00:16:15 +0800

    标题加粗

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -52,6 +52,7 @@ $border-color: #d9d9d9;
   border: 1px solid $border-color;
   margin: 16px 0 32px;
   > h2 {
+    font-weight: bold;
     font-size: 20px;
     padding: 8px 16px;
     border-bottom: 1px solid $border-color;
