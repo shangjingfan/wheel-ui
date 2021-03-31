cc459f9c1c0136928dc15dd7233ae10698a5e009
shangjingfan <876625909@qq.com>
2021-03-31 15:41:28 +0800

    完成Collapse组件

--- src/lib/Collapse.vue
+++ src/lib/Collapse.vue
@@ -1,13 +1,85 @@
 <template>
-  <div>
-   Collapse组件
+  <div class="wheel-coll">
+    <div class="wheel-coll-title" @click="showContent" :class="{visible: visible}">
+      {{title}}
+      <svg class="icon">
+        <use xlink:href="#icon-right"></use>
+      </svg>
+    </div>
+    <transition name="fade">
+    <div class="wheel-coll-content" v-if="visible" :class="{visible: visible}">
+      <slot/>
+    </div>
+    </transition>
   </div>
 </template>
 
 <script lang="ts">
-  export default {};
+  import {ref} from 'vue';
+
+  export default {
+    props: {
+      title: {type: String, required: true}
+    },
+    setup() {
+      let visible = ref(false);
+      const showContent = () => {
+        visible.value = !visible.value;
+      };
+      return {showContent, visible};
+    }
+  };
 </script>
 
 <style scoped lang="scss">
+  $color: #d9d9d9;
+  .wheel-coll {
+    padding: 8px 16px;
+    border-left: 2px solid $color;
+    &-title {
+      padding: 8px 8px 8px 0;
+      border-bottom: 1px solid #d9d9d9;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
 
+      svg {
+        color: #d9d9d9;
+        transition: 0.5s;
+      }
+
+      &.visible {
+        border-bottom: none;
+
+        svg {
+          transform: rotate(90deg);
+        }
+      }
+
+      &:hover {
+        cursor: pointer;
+        color: #409eff;
+
+        svg {
+          color: #409eff;
+        }
+      }
+    }
+
+    &-content {
+      padding: 8px 0;
+      &.visible {
+        border-bottom: 1px solid #d9d9d9;
+      }
+    }
+  }
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 1s ease;
+  }
+
+  .fade-enter-from,
+  .fade-leave-to {
+    opacity: 0;
+  }
 </style>
