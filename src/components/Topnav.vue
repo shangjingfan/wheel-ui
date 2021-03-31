7311dae442257e94ae2b6209c8f40ee0d1ac8c55
shangjingfan <876625909@qq.com>
2021-03-31 12:20:58 +0800

    车轮滚滚

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -20,76 +20,98 @@
   </div>
 </template>
 <script lang="ts">
-import { inject, Ref } from "vue";
-export default {
-  props: {
-    toggleMenuButtonVisible: {
-      type: Boolean,
-      default: false,
+  import {inject, Ref} from 'vue';
+
+  export default {
+    props: {
+      toggleMenuButtonVisible: {
+        type: Boolean,
+        default: false,
+      },
     },
-  },
-  setup() {
-    const asideVisible = inject<Ref<boolean>>("asideVisible"); //get
-    const toggleAside = () => {
-      asideVisible.value = !asideVisible.value;
-    };
-    return { toggleAside };
-  },
-};
+    setup() {
+      const asideVisible = inject<Ref<boolean>>('asideVisible'); //get
+      const toggleAside = () => {
+        asideVisible.value = !asideVisible.value;
+      };
+      return {toggleAside};
+    },
+  };
 </script>
 <style lang="scss" scoped>
-$color: #030507;
-.topnav {
-  background: #b6dff9;
-  color: $color;
-  display: flex;
-  padding: 16px;
-  position: fixed;
-  left: 0;
-  top: 0;
-  width: 100%;
-  z-index: 20;
-  justify-content: center;
-  align-items: center;
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
-    > svg {
-      width: 32px;
-      height: 32px;
-    }
-  }
-  > .menu {
+  $color: #030507;
+  .topnav {
+    background: #b6dff9;
+    color: $color;
     display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      &:hover {
-        color: blue;
-      }
-      margin: 0 1em;
-    }
-  }
-  > .toggleAside {
-    display: inline-block;
-    width: 32px;
-    height: 32px;
-    position: absolute;
-    left: 16px;
-    top: 50%;
-    transform: translateY(-50%);
-    display: none;
-  }
-  @media (max-width: 500px) {
-    > .menu {
-      display: none;
-    }
+    padding: 16px;
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    z-index: 20;
+    justify-content: center;
+    align-items: center;
+
     > .logo {
-      margin: auto;
+      max-width: 6em;
+      margin-right: auto;
+
+      > svg {
+        width: 32px;
+        height: 32px;
+        animation: wheel 2500ms infinite linear;
+        &:hover{
+
+        }
+      }
+
     }
+
+    > .menu {
+      display: flex;
+      white-space: nowrap;
+      flex-wrap: nowrap;
+
+      > li {
+        &:hover {
+          color: blue;
+        }
+
+        margin: 0 1em;
+      }
+    }
+
     > .toggleAside {
       display: inline-block;
+      width: 32px;
+      height: 32px;
+      position: absolute;
+      left: 16px;
+      top: 50%;
+      transform: translateY(-50%);
+      display: none;
+    }
+
+    @media (max-width: 500px) {
+      > .menu {
+        display: none;
+      }
+      > .logo {
+        margin: auto;
+      }
+      > .toggleAside {
+        display: inline-block;
+      }
     }
   }
-}
+
+  @keyframes wheel {
+    0% {
+      transform: rotate(0);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 </style>
